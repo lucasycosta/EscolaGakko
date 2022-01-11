549f504f7c2d6389c2e20dc88e545e2e4bcdd5a5
Lucas Yamaguti <lucasycosta@gmail.com>
2022-01-11 20:19:55 -0300

    criação do repositorio

---
+++ src/main/java/com/lucas/escolaGakko/EscolaGakkoApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.escolaGakko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaGakkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaGakkoApplication.class, args);
+	}
+
+}
